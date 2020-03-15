4fbdfc16e7fb7215f724ed613ad618245709f899
61sultan88 <40860235+61sultan88@users.noreply.github.com>
2020-03-15 12:24:43 +0530

    Add files via upload

---
+++ ALL TOOLS.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+echo $red 
+ echo ""----------------------------------------------------------------------------- ""
+echo " This Script is written by ADTECH "
+echo "---------------------------------------------------------------------------------
+apt-get update -y                      #update in progress
+apt install python -y           #installation......
+apt install python2 -y
+apt install ruby -y 
+apt install git -y
+apt install screenfetch -y
+apt install cowsay -y
+apt install toilet -y
+apt install figlet -y
+echo  ":-):-):-):-):-)"
+echo   " Subscribe the Channel/      "               
+ echo   " ADTECH "                                                                    
+echo  ":-):-):-):-):-)"
+
+apt install php -y 
+apt install perl -y
+apt install nmap -y
+pkg install openssh -y
+echo  "#####################################"
+echo   "HACKERS "               
+ echo   " join us on facebook ADTECH"                                                                   echo   "####################################"
+
+apt install bash -y
+apt install clang -y
+apt install jq -y
+apt install macchanger -y
+
+echo " "                                                                   
+echo   "####################################"
+
+apt install nano -y
+apt install curl -y
+apt install tar -y
+apt install zip -y
+apt install fish -y
+                                                            echo   " "
+                                                            
+apt install unzip -y
+apt install tor -y 
+apt install sudo -y
+apt install wget -y
+apt install tor -y
+apt install w3m -y
+apt install cmatrix -y
+echo  "#####################################"
+echo   " we are hackers "               
+                                                                  echo   "####################################"
+
+apt install wcalc -y 
+apt install openssl
+apt install bmon -y
+
+echo  "#####################################"
+               
+echo   " processing wait......
+"                                                                    
+echo   "####################################"
+
+exit
